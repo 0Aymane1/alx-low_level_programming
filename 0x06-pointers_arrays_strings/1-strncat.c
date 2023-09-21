a3f61ae31dae7912b3ad9278b5e36fe45d7be97f
0Aymane1 <aymanhamdaoui2002@gmail.com>
2023-09-21 10:52:02 -0700

    1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strcat - a function that contains two strings
+ *
+ * @dest: pointer to destination
+ * @src: pointer to source input
+ * @n: most number of bytes from @src
+ *
+ * Return: @dest
+ */
+
+char *_strcat(char *dest, char *src, int n)
+{
+	int c, i;
+
+	c = 0;
+
+	while (dest[c])
+		c++;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[c + i] = src[i];
+
+	dest[c + i] = '\0';
+
+	return (dest);
+}
