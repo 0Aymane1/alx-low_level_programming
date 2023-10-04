c81bffaf68d1223c5349607da5fcf82b5e788aa4
0Aymane1 <aymanhamdaoui2002@gmail.com>
2023-10-04 09:16:20 -0700

    0-create_array.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,37 +2,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * str_concat - concatenates two strings.
- * @s1: string1
- * @s2: string2
+ * _strdup - a copy of the string
+ * @str: the string given
  * Return: buff
  * on error NULL returned
  */
-char *str_concat(char *s1, char *s2)
+char *_strdup(char *str)
 {
 	char *buff;
-	int i, j = 0, l = 0;
+	int i, l = 0;
 
-	if (s1 != NULL)
-	{
-		while (s1[j] != '\0')
-			j++;
-	}
-	if (s2 != NULL)
-	{
-		while (s2[l] != '\0')
-			l++;
-	}
-	buff = malloc((l + j + 1) * sizeof(char));
+	if (str == NULL)
+		return (NULL);
+
+	while (str[l] != '\0')
+		l++;
+
+	buff = malloc((l + 1) * sizeof(char));
 	if (buff == NULL)
 		return (NULL);
 
-	for (i = 0; i < j; i++)
-		buff[i] = s1[i];
-
 	for (i = 0; i < l; i++)
-		buff[j + i] = s2[i];
+		buff[i] = str[i];
 
-	buff[j + l] = '\0';
 	return (buff);
-}
+}	
